5c629c7296145fd51bc4cc28436898c949800a36
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-03-09 19:28:56 +0300

    feat: map microtask

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MapTask from "./MapTask";
+import Button from "./components/Button";
+
+
+const topCars = [
+  {manufacturer:'BMW', model:'m5cs'},
+  {manufacturer:'Mercedes', model:'e63s'},
+  {manufacturer:'Audi', model:'rs6'}
+];
+
+
+const button1Foo = (subscriber: string, age: number)=>{
+    console.log(subscriber, age)
+};
+const button2Foo = (subscriber: string)=>{
+    console.log(subscriber)
+};
+const button3Foo = ()=>{
+  console.log('Im stupid button')
+};
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapTask topCars={topCars}/>
+
+      <Button name={'MyYouTubeChanel-1'} callBack={()=>button1Foo('Im Vasya', 21)}/>
+      <Button name={'MyYouTubeChanel-2'} callBack={()=>button2Foo('Im Ivan')}/>
+      <Button name={'Stupid Button'} callBack={button3Foo}/>
     </div>
   );
 }
