198dcff608467185f872cbe35800a5d973e54054
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-03-09 19:40:35 +0300

    feat: add filter microtask

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import MapTask from "./MapTask";
 import Button from "./components/Button";
+import FilterTask from "./components/FilterTaskt";
 
 
 const topCars = [
@@ -24,14 +25,16 @@ const button3Foo = ()=>{
 function App() {
 
   return (
-    <div className="App">
-      <MapTask topCars={topCars}/>
-
-      <Button name={'MyYouTubeChanel-1'} callBack={()=>button1Foo('Im Vasya', 21)}/>
-      <Button name={'MyYouTubeChanel-2'} callBack={()=>button2Foo('Im Ivan')}/>
-      <Button name={'Stupid Button'} callBack={button3Foo}/>
-    </div>
+    // <div className="App">
+    //   <MapTask topCars={topCars}/>
+    //
+    //   <Button name={'MyYouTubeChanel-1'} callBack={()=>button1Foo('Im Vasya', 21)}/>
+    //   <Button name={'MyYouTubeChanel-2'} callBack={()=>button2Foo('Im Ivan')}/>
+    //   <Button name={'Stupid Button'} callBack={button3Foo}/>
+    // </div>
+      <FilterTask/>
   );
+
 }
 
 export default App;
